b2ae969674c274ce0905070103edda2a87ef2b76
Marcin Aman <marcin.aman@comarch.pl>
2019-02-12 10:09:16 +0100

    dev

--- src/main/scala/CsvParser.scala
+++ src/main/scala/CsvParser.scala
@@ -3,7 +3,10 @@ import scala.io
 
 object CsvParser {
   def parse(file: io.Source): collection.immutable.Iterable[record[Int,String]] = {
-    file.getLines().filter(e => !e.trim().equals("")).map(e => parseSingleRecord(e)).toList
+    file.getLines()
+      .filter(e => !e.trim().equals(""))
+      .map(e => parseSingleRecord(e))
+      .to[collection.immutable.Iterable]
   }
 
   private def parseSingleRecord(content: String): record[Int, String] = record(0, content)
